d0cc7377acfe8587d018720005b7c8de887e9dd6
Nicholas Terry <nick.i.terry@gmail.com>
2014-01-02 18:29:55 -0700

    Added basic endianness check and handle. Also added start index to methods and implemented them for float and double

--- Type2Byte/BaseConverters/B2T.cs
+++ Type2Byte/BaseConverters/B2T.cs
@@ -9,6 +9,13 @@ namespace Type2Byte.BaseConverters
 {
     public class B2T
     {
+        public static bool IsLittleEndian { get; private set; }
+
+        static B2T()
+        {
+            IsLittleEndian = DetermineIfLittleEndian();
+        }
+
         public static T Get<T>(byte[] value)
         {
             if (typeof(T) == typeof(bool)) return (T)(object)GetBool(value);
@@ -27,112 +34,130 @@ namespace Type2Byte.BaseConverters
             throw new DataException("Provided type ({0}) cannot be deserialized from given value. You must provide a valuetype (except struct and enum) or a string");
         }
 
-        public static bool IsLittleEndian()
+        //TODO: Need to handle endianness. All information is encoded in little-endian style
+        #region private getters
+
+        private static bool DetermineIfLittleEndian()
         {
             var test = T2B.ToBytes('a');
             var result = Convert.ToChar((test[0] | test[1] << 8));
             return (result == 'a');
         }
 
-        //TODO: Need to handle endianness. All information is encoded in little-endian style
-        #region private getters
-
-        private static bool GetBool(byte[] value)
+        private static bool GetBool(byte[] value, int startIndex = 0)
         {
             if (value == null) throw new ArgumentNullException("value");
             if (value.Length != sizeof(bool)) throw new DataException("Provided data does not appear to be of type bool");
+            HandleEndianness(value);
             return (value.First() != 0);
         }
 
-        private static byte GetByte(byte[] value)
+        private static byte GetByte(byte[] value, int startIndex = 0)
         {
             if (value == null) throw new ArgumentNullException("value");
             if (value.Length != sizeof(byte)) throw new DataException("Provided data does not appear to be of type byte");
+            HandleEndianness(value);
             return value.First();
         }
 
-        private static sbyte GetSByte(byte[] value)
+        private static sbyte GetSByte(byte[] value, int startIndex = 0)
         {
             if (value == null) throw new ArgumentNullException("value");
             if (value.Length != sizeof(sbyte)) throw new DataException("Provided data does not appear to be of type sbyte");
+            HandleEndianness(value);
             return (sbyte)value.First();
         }
 
-        private static char GetChar(byte[] value)
+        private static char GetChar(byte[] value, int startIndex = 0)
         {
             if (value == null) throw new ArgumentNullException("value");
             if (value.Length != sizeof(char)) throw new DataException("Provided data does not appear to be of type char");
+            HandleEndianness(value);
             return Convert.ToChar((value[0] | value[1] << 8));
         }
 
-        private static float GetFloat(byte[] value)
+        private static float GetFloat(byte[] value, int startIndex = 0)
         {
             if (value == null) throw new ArgumentNullException("value");
             if (value.Length != sizeof(float)) throw new DataException("Provided data does not appear to be of type float");
+            HandleEndianness(value);
             var floatArray = new float[1];
-            Buffer.BlockCopy(value, 0, floatArray, 0, value.Length);
+            Buffer.BlockCopy(value, startIndex, floatArray, 0, value.Length - startIndex);
             return floatArray[0];
         }
 
-        private static double GetDouble(byte[] value)
+        private static double GetDouble(byte[] value, int startIndex = 0)
         {
             if (value == null) throw new ArgumentNullException("value");
             if (value.Length != sizeof(double)) throw new DataException("Provided data does not appear to be of type double");
+            HandleEndianness(value);
             var doubleArray = new double[1];
-            Buffer.BlockCopy(value, 0, doubleArray, 0, value.Length);
+            Buffer.BlockCopy(value, startIndex, doubleArray, 0, value.Length - startIndex);
             return doubleArray[0];
         }
 
-        private static int GetInt(byte[] value)
+        private static int GetInt(byte[] value, int startIndex = 0)
         {
             if (value == null) throw new ArgumentNullException("value");
             if (value.Length != sizeof(int)) throw new DataException("Provided data does not appear to be of type int");
+            HandleEndianness(value);
             return (value[0] << 0) | (value[1] << 8) | (value[2] << 16) | (value[3] << 24);
         }
 
-        private static uint GetUInt(byte[] value)
+        private static uint GetUInt(byte[] value, int startIndex = 0)
         {
             if (value == null) throw new ArgumentNullException("value");
             if (value.Length != sizeof(uint)) throw new DataException("Provided data does not appear to be of type uint");
+            HandleEndianness(value);
             return Convert.ToUInt32((value[0] << 0) | (value[1] << 8) | (value[2] << 16) | (value[3] << 24));
         }
 
-        private static long GetLong(byte[] value)
+        private static long GetLong(byte[] value, int startIndex = 0)
         {
             if (value == null) throw new ArgumentNullException("value");
             if (value.Length != sizeof(long)) throw new DataException("Provided data does not appear to be of type long");
+            HandleEndianness(value);
             return Convert.ToInt64((value[0] << 0) | (value[1] << 8) | (value[2] << 16) | (value[3] << 24 | value[4] << 32) | (value[5] << 40) | (value[6] << 48) | (value[7] << 56));
         }
 
-        private static ulong GetULong(byte[] value)
+        private static ulong GetULong(byte[] value, int startIndex = 0)
         {
             if (value == null) throw new ArgumentNullException("value");
             if (value.Length != sizeof(ulong)) throw new DataException("Provided data does not appear to be of type ulong");
+            HandleEndianness(value);
             return Convert.ToUInt64((value[0] << 0) | (value[1] << 8) | (value[2] << 16) | (value[3] << 24 | value[4] << 32) | (value[5] << 40) | (value[6] << 48) | (value[7] << 56));
         }
 
-        private static short GetShort(byte[] value)
+        private static short GetShort(byte[] value, int startIndex = 0)
         {
             if (value == null) throw new ArgumentNullException("value");
             if (value.Length != sizeof(ulong)) throw new DataException("Provided data does not appear to be of type ulong");
+            HandleEndianness(value);
             return Convert.ToInt16((value[0] << 0) | (value[1] << 8));
         }
 
-        private static ushort GetUShort(byte[] value)
+        private static ushort GetUShort(byte[] value, int startIndex = 0)
         {
             if (value == null) throw new ArgumentNullException("value");
             if (value.Length != sizeof(ulong)) throw new DataException("Provided data does not appear to be of type ulong");
+            HandleEndianness(value);
             return Convert.ToUInt16((value[0] << 0) | (value[1] << 8));
         }
 
-        private static string GetString(byte[] value)
+        private static string GetString(byte[] value, int startIndex = 0)
         {
             var sb = new StringBuilder();
+            HandleEndianness(value);
             foreach (var @char in value)
             {
                 sb.Append(@char);
             }
             return sb.ToString();
+        }
+
+        private static void HandleEndianness(byte[] byteArray)
+        {
+            if (!IsLittleEndian) Array.Reverse(byteArray);
         }
         #endregion
     }
